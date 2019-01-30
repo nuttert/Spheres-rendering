8ca8b32077ba41fe359fc9ab03afb56346ba513d
Vlad <vlad.mark-2013@mail.ru>
2019-01-30 12:13:07 +0300

    Initial Commit

---
+++ Rendering/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rendering
+//
+//  Created by vlad on 30.01.2019.
+//  Copyright © 2019 vlad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
